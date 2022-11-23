5a32280ebf9a5c9c38f053e7706186b0eda44136
Lucky Saggu <lss007@yahoo.com>
2022-11-23 11:19:12 +0530

    code Updated

--- resources/views/livewire/common/header.blade.php
+++ resources/views/livewire/common/header.blade.php
@@ -28,7 +28,7 @@
                           <li><a href="{{route('CenterbewachungPage')}}" class="@if(Route::is('CenterbewachungPage') ) active @else '' @endif">Centerbewachung</a></li>
                           <li><a href="{{route('KaufhausdetektivePage')}}" class="@if(Route::is('KaufhausdetektivePage') ) active @else '' @endif">Kaufhausdetektive</a></li>
                           <li><a href="{{route('baustellenbewachungPage')}}" class="@if(Route::is('baustellenbewachungPage') ) active @else '' @endif">Baustellenbewachung</a></li>
-                          <li><a href="{{route('openingAndclosingPage')}}" class="@if(Route::is('openingAndclosingPage') ) active @else '' @endif">Öffnungs- und Schließddienst</a></li>
+                          <li><a href="{{route('openingAndclosingPage')}}" class="@if(Route::is('openingAndclosingPage') ) active @else '' @endif">Öffnungs-und Schließddienst</a></li>
                           <li><a href="{{route('revierfahrtenPage')}}" class="@if(Route::is('revierfahrtenPage') ) active @else '' @endif">Revierkontrollen</a></li>
                           <li><a href="{{route('empfangsdienst')}}" class="@if(Route::is('empfangsdienst') ) active @else '' @endif">Empfangsdienst</a></li>
                           <li><a href="{{route('ShopGuardPage')}}" class="@if(Route::is('ShopGuardPage') ) active @else '' @endif">Shop Guard</a></li>
