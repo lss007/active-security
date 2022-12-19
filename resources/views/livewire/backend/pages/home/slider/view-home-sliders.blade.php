c531a50115a7b272e4c622d20360b11be184c149
Lucky Saggu <lss007@yahoo.com>
2022-12-19 15:40:06 +0530

    updated

--- resources/views/livewire/backend/pages/home/slider/view-home-sliders.blade.php
+++ resources/views/livewire/backend/pages/home/slider/view-home-sliders.blade.php
@@ -96,17 +96,16 @@
                       </span>
                     </p>
                     <p class="mg-b-5">
-                      @if(  isset($slider->link))
-                      <span class="text-primary"> Custom Link : </span>
-                      <a href="{{route($slider->link)}}" class="btn btnPrimary2">{!! isset($slider->button_text) ? $slider->button_text : "mehr erfahren"!!}</a>
-                      
-                      @else
-                      <span class="text-primary"> Route Link : </span>
-
-                      <a href="{{$slider->custom_Link}}" class="btn btnPrimary2">{!! isset($slider->button_text) ? $slider->button_text : "mehr erfahren"!!} </a>
-                      @endif
-
-              
+                   
+              @if(isset($slider->link))
+              <span class="text-primary"> Route Link : </span>
+               <a href="{{route($slider->link)}}" class="btn btnPrimary2">{!! isset($slider->button_text) ? $slider->button_text : "mehr erfahren"!!}</a>
+               @else
+             
+               <span class="text-primary"> Custom Link : </span>
+               <a href="{{$slider->custom_Link}}" class="btn btnPrimary2">{!! isset($slider->button_text) ? $slider->button_text : "mehr erfahren"!!} </a>
+     
+                @endif
                     </p>
                     <div class="row">
                       <div class="col-md-4">
