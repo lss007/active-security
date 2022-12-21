2b4539b9d7dc165249f65dc430e3fe4b514e299a
Lucky Saggu <lss007@yahoo.com>
2022-12-21 17:01:01 +0530

    validate

--- app/Http/Livewire/Backend/Pages/Navbar/EditSubNavbar.php
+++ app/Http/Livewire/Backend/Pages/Navbar/EditSubNavbar.php
@@ -30,13 +30,16 @@ class EditSubNavbar extends Component
         }
      }
      protected $rules = [
-        'navbarLink'         => 'required',
-        'routeName'         => 'required',
-
+        'navbarLink'  => 'required',
+        'routeName'   => 'required',
+        'link'       => 'required',
+        // 'link'       => 'required|unique:sub_navbars,route_link,',
     ];
     protected $messages = [
         'navbarLink.required' => 'Main Navbar link field is required.',
         'routeName.required' => 'Sublink Name field is required.',
+        // 'link.unique' => 'Route link has already been taken.',
+
 
     ]; 
 
@@ -46,7 +49,7 @@ class EditSubNavbar extends Component
                     $SaveSub =  SubNavbar::find($this->subLinkId);
                     $SaveSub->navbar_id = $this->navbarLink;
                     $SaveSub->route_name = $this->routeName;
-                    // $SaveSub->route_link  =  $this->link;
+                    $SaveSub->route_link  =  $this->link;
 
                     $SaveSub->save();
                     $notification = array(
