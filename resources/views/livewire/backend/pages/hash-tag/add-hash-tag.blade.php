5a4de0e7bf8a2fe8bebf1ec0b80c51100b3b43ab
Lucky Saggu <lss007@yahoo.com>
2022-12-28 17:49:13 +0530

    changes

--- resources/views/livewire/backend/pages/hash-tag/add-hash-tag.blade.php
+++ resources/views/livewire/backend/pages/hash-tag/add-hash-tag.blade.php
@@ -9,7 +9,7 @@
   
     <div class="card pd-20 pd-sm-40">
         <h6 class="card-body-title">  
-            <a href="{{route('view_all_banner')}}" class="btn btn-teal active mg-b-10"  >Back</a>
+            <a href="{{route('view_Allhashtag')}}" class="btn btn-teal active mg-b-10"  >Back</a>
         </h6>
         <form  wire:submit.prevent="saveHashtag">
              <div class="form-layout">
@@ -50,7 +50,7 @@
                </div><!-- row -->
      
                <div class="form-layout-footer">
-                 <button class="btn btn-info mg-r-5" type="submit" wire:loading.attr="disabled">Update Banner</button>
+                 <button class="btn btn-info mg-r-5" type="submit" wire:loading.attr="disabled">Update </button>
                 <div  wire:loading class="pd-x-20">
                     <img src="https://paladins-draft.com/img/circle_loading.gif" width="64px" height="64" >
                 </div>
