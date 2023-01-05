9e74fa0fb7cd7c657c932d2f9571d3f4ad96c547
Lucky Saggu <lss007@yahoo.com>
2023-01-05 17:29:59 +0530

    font removed

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -21,23 +21,277 @@
       {{-- start frontend css  --}}
     <link href="{{ asset('frontend/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('frontend/css/bootstrap.min.css.map') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('frontend/css/swiper-bundle.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('frontend/css/toastr.css') }}" rel="stylesheet" type="text/css" />
-    @livewireStyles
+    <link href="{{ asset('frontend/css/style.css') }}" rel="stylesheet" type="text/css" />
+  
       <style>
-        .menuColMain, .bannerContent, section {
+
+
+/* @font-face {
+  font-family: 'Poppins';
+  src: url('{{ asset('fonts/Poppins-Medium.eot') }}');
+  src: url('{{ asset('fonts/Poppins-Medium.eot?#iefix') }}') format('embedded-opentype'),
+      url('{{ asset('fonts/Poppins-Medium.woff2') }}') format('woff2'),
+      url('{{ asset('fonts/Poppins-Medium.woff') }}') format('woff'),
+      url('{{ asset('fonts/Poppins-Medium.ttf') }}') format('truetype'),
+      url('{{ asset('fonts/Poppins-Medium.svg#Poppins-Medium') }}') format('svg');
+  font-weight: 500;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Poppins';
+  src: url('{{ asset('fonts/Poppins-SemiBold.eot') }}');
+  src: url('{{ asset('fonts/Poppins-SemiBold.eot?#iefix') }}') format('embedded-opentype'),
+      url('{{ asset('fonts/Poppins-SemiBold.woff2') }}') format('woff2'),
+      url('{{ asset('fonts/Poppins-SemiBold.woff') }}') format('woff'),
+      url('{{ asset('fonts/Poppins-SemiBold.ttf') }}') format('truetype'),
+      url('{{ asset('fonts/Poppins-SemiBold.svg#Poppins-SemiBold') }}') format('svg');
+  font-weight: 600;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Poppins';
+  src: url('{{ asset('fonts/Poppins-Bold.eot') }}');
+  src: url('{{ asset('fonts/Poppins-Bold.eot?#iefix') }}') format('embedded-opentype'),
+      url('{{ asset('fonts/Poppins-Bold.woff2') }}') format('woff2'),
+      url('{{ asset('fonts/Poppins-Bold.woff') }}') format('woff'),
+      url('{{ asset('fonts/Poppins-Bold.ttf') }}') format('truetype'),
+      url('{{ asset('fonts/Poppins-Bold.svg#Poppins-Bold') }}') format('svg');
+  font-weight: bold;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Poppins';
+  src: url('{{ asset('fonts/Poppins-Regular.eot') }}');
+  src: url('{{ asset('fonts/Poppins-Regular.eot?#iefix') }}') format('embedded-opentype'),
+      url('{{ asset('fonts/Poppins-Regular.woff2') }}') format('woff2'),
+      url('{{ asset('fonts/Poppins-Regular.woff') }}') format('woff'),
+      url('{{ asset('fonts/Poppins-Regular.ttf') }}') format('truetype'),
+      url('{{ asset('fonts/Poppins-Regular.svg#Poppins-Regular') }}') format('svg');
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
+:root {
+  --baseFont: 'Poppins';
+}
+body {
+	font-family: 'Poppins';
+} */
+
+        /* .menuColMain, .bannerContent, section {
   opacity: 0;
   transition: all 400ms ease-in-out 0s;
 }
 .homeload .menuColMain,.homeload .bannerContent, .homeload section {
   opacity: 1;
+} */
+
+/* loader */
+/*
+html {
+  overflow-y: hidden;
 }
+html.homeload {
+  overflow-y: initial;
+}
+.homeload .loaderCol {
+  opacity: 0;
+  visibility: hidden;
+  pointer-events: none;
+}
+.loaderCol {
+	position: fixed;
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+	display: -webkit-flex;
+	display: -ms-flex;
+	display: flex;
+	-ms-align-items: center;
+	align-items: center;
+	justify-content: center;
+	background-color: #f9feff;
+  -webkit-transition: all 500ms ease-in-out 0s;
+  -o-transition: all 500ms ease-in-out 0s;
+  transition: all 500ms ease-in-out 0s;
+  z-index: 999;
+}
+.preloader-orbit-loading {
+  position: relative;
+  width: 250px;
+  height: 250px;
+  border-radius: 50%;
+  -o-border-radius: 50%;
+  -ms-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  display: -webkit-flex;
+  display: -ms-flex;
+  display: flex;
+  -ms-align-items: center;
+  align-items: center;
+  justify-content: center;
+}
+.preloader-orbit-loading > img {
+	width: 80px;
+  -webkit-animation-name: pulse;
+  animation-name: pulse;
+  -webkit-animation-duration: 3s;
+  animation-duration: 3s;
+  -webkit-animation-iteration-count: infinite;
+  animation-iteration-count: infinite;
+  transform: scale(.8);
+}
+@keyframes pulse {
+  0% {
+    transform: scale(.8);
+    opacity: 0.8;
+  }
+  50% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  100% {
+    transform: scale(.8);
+    opacity: 0.8;
+  }
+}
+.preloader-orbit-loading .cssload-inner {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  -o-box-sizing: border-box;
+  -ms-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  border-radius: 50%;
+  -o-border-radius: 50%;
+  -ms-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+}
+.preloader-orbit-loading .cssload-inner.cssload-one,
+.preloader-orbit-loading .cssload-inner.cssload-two,
+.preloader-orbit-loading .cssload-inner.cssload-three {
+  -webkit-animation-duration: 2.15s;
+  animation-duration: 2.15s;
+  -webkit-animation-timing-function: linear;
+  animation-timing-function: linear;
+  -webkit-animation-iteration-count: infinite;
+  animation-iteration-count: infinite;
+}
+.preloader-orbit-loading .cssload-inner.cssload-one {
+  left: 0%;
+  top: 0%;
+  -webkit-animation-name: cssload-rotate-one;
+  animation-name: cssload-rotate-one;
+  border-bottom: 3px solid var(--primaryColor);
+}
+.preloader-orbit-loading .cssload-inner.cssload-two {
+  right: 0%;
+  top: 0%;
+  -webkit-animation-name: cssload-rotate-two;
+  animation-name: cssload-rotate-two;
+  border-right: 3px solid var(--secondaryColor);
+}
+.preloader-orbit-loading .cssload-inner.cssload-three {
+  right: 0%;
+  bottom: 0%;
+  -webkit-animation-name: cssload-rotate-three;
+  animation-name: cssload-rotate-three;
+  border-top: 3px solid #008143;
+}
+
+@keyframes cssload-rotate-one {
+  0% {
+    transform: rotateX(35deg) rotateY(-45deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(35deg) rotateY(-45deg) rotateZ(360deg);
+  }
+}
+@-webkit-keyframes cssload-rotate-one {
+  0% {
+    transform: rotateX(35deg) rotateY(-45deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(35deg) rotateY(-45deg) rotateZ(360deg);
+  }
+}
+@keyframes cssload-rotate-two {
+  0% {
+    transform: rotateX(50deg) rotateY(10deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(50deg) rotateY(10deg) rotateZ(360deg);
+  }
+}
+@-webkit-keyframes cssload-rotate-two {
+  0% {
+    transform: rotateX(50deg) rotateY(10deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(50deg) rotateY(10deg) rotateZ(360deg);
+  }
+}
+@keyframes cssload-rotate-three {
+  0% {
+    transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
+  }
+}
+@-webkit-keyframes cssload-rotate-three {
+  0% {
+    transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
+  }
+}*/
+
+
+/* .loaderCol {
+  background-color: #32445d;
+}
+.preloader-orbit-loading > img {
+    filter: grayscale(1) contrast(0) brightness(6);
+}
+.preloader-orbit-loading .cssload-inner.cssload-one {
+    border-bottom: 3px solid #ffffff;
+}
+.preloader-orbit-loading .cssload-inner.cssload-two {
+    border-right: 3px solid #ffffff;
+}
+.preloader-orbit-loading .cssload-inner.cssload-three {
+    border-top: 3px solid #ffffff;
+} */
+/* --------------------------------------------- */
+
       </style>
   {{-- end frontend css  --}}
+  @livewireStyles
 </head>
 <body>
-
+  {{-- <div class="loaderCol">
+    <div class="preloader-orbit-loading">
+      <img src="{{ asset('frontend/images/logo-icon.png')}}" alt="...">
+      <div class="cssload-inner cssload-one"></div>
+      <div class="cssload-inner cssload-two"></div>
+      <div class="cssload-inner cssload-three"></div>
+    </div>
+  </div> --}}
  
      @include('livewire.common.header')
        <!-- Page Content -->
