b03d6137cdcf98ca38970a3983145c08b52419c6
Lucky Saggu <lss007@yahoo.com>
2023-01-06 11:33:17 +0530

    extra h1 removed

--- resources/views/livewire/common/footer.blade.php
+++ resources/views/livewire/common/footer.blade.php
@@ -136,7 +136,7 @@
           <div class="modal-dialog modal-xl">
             <div class="modal-content">
               <div class="modal-header">
-                <h1 class="modal-title" id="privacyModalLabel">Privatsphäre-Einstellungen</h1>
+                <h2 class="modal-title" id="privacyModalLabel">Privatsphäre-Einstellungen</h2>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
